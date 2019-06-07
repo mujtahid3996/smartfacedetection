8c8974760bd9139245111e17a3a80f99c4d0f7c0
mujtahid3996 <mujtahid2018@gmail.com>
2019-06-08 00:33:51 +0600

    no comments

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
-const app = express();
 const cors = require('cors');
 const knex = require('knex');
 const register =  require('./controllers/register');
@@ -15,7 +14,6 @@ const db = knex({
         ssl: true,
     }
   });
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
